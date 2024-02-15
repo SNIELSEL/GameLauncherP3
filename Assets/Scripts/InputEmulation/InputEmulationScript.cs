9ae58f5454af5880dd48b81ff2fc29538e49ca6e
SNIELSEL <niels@haverkotte.nl>
2024-02-15 16:50:54 +0100

    Emulation af

--- Assets/Scripts/InputEmulation/InputEmulationScript.cs
+++ Assets/Scripts/InputEmulation/InputEmulationScript.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using WindowsInput.Native;
 using WindowsInput;
+using System.IO;
+using Unity.VisualScripting;
 
 public class InputEmulationScript : MonoBehaviour
 {
     InputSimulator sim;
-    public void Start()
+    [SerializeField] VirtualKeyCode[] ArcadeInputs;
+    [SerializeField] string[] InputNames;
+    [SerializeField] string filePath, fileName;
+    private int currentLineIndex = 0;
+
+    void Start()
     {
         sim = new InputSimulator();
+
+        filePath = Application.dataPath + fileName;
+
+        for (int i = 0; i < ArcadeInputs.Length; i++)
+        {
+            InputNames[i] = GetLineAtIndex(i);
+            ArcadeInputs[i] = (VirtualKeyCode)System.Enum.Parse(typeof(VirtualKeyCode), GetLineAtIndex(i));
+        }
     }
 
-    public void Update()
+    private string GetLineAtIndex(int index)
+    {
+        string[] lines = File.ReadAllLines(filePath);
+
+        if(index < lines.Length)
+        {
+            return lines[index];
+        }
+        else
+        {
+            return "No Lines";
+        }
+    }
+
+    void Update()
     {
         if (Input.GetMouseButton(0))
         {
