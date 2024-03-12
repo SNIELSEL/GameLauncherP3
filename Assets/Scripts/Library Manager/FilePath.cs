fdaeecaab7f89709598251762e0975d4d68c4be4
Dylano Burgers <dylano.burgers@outlook.com>
2024-03-12 14:21:31 +0100

    Alles werkt en is af en werkt
    het moet a;lleen nog worden gelinkt in de main scene

---
+++ Assets/Scripts/Library Manager/FilePath.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+using SimpleFileBrowser;
+
+public class FilePath : MonoBehaviour
+{
+    [SerializeField] private string fileName;
+    [SerializeField] private string[] subFolders;
+
+    public DescriptionAutoFill descriptionAutoFill;
+
+
+    public void ChooseGamesFolder()
+    {
+        // Add a new quick link to the browser (optional) (returns true if quick link is added successfully)
+        // It is sufficient to add a quick link just once
+        // Name: Users
+        // Path: C:\Users
+        // Icon: default (folder icon)
+        FileBrowser.AddQuickLink("Users", "C:\\Users", null);
+
+        // !!! Uncomment any of the examples below to show the file browser !!!
+
+        // Example 1: Show a save file dialog using callback approach
+        // onSuccess event: not registered (which means this dialog is pretty useless)
+        // onCancel event: not registered
+        // Save file/folder: file, Allow multiple selection: false
+        // Initial path: "C:\", Initial filename: "Screenshot.png"
+        // Title: "Save As", Submit button text: "Save"
+        FileBrowser.ShowLoadDialog(OnFilesSelected, null, FileBrowser.PickMode.FilesAndFolders, false, "C:\\Users\\" + System.Environment.UserName + "\\Desktop", "", "Choose Games Folder", "Load");
+    }
+
+    void OnFilesSelected(string[] filePaths)
+    {
+        // Print paths of the selected files
+        for (int i = 0; i < filePaths.Length; i++)
+        {
+            Debug.Log(filePaths[i]);
+        }
+
+        // Get the file path of the first selected file
+        fileName = filePaths[0];
+
+        PlayerPrefs.SetString("GameFolderPath", fileName);
+
+        GameFolderCheck();
+    }
+
+    private void Awake()
+    {
+        fileName = PlayerPrefs.GetString("GameFolderPath");
+
+        if(fileName == null || fileName == "")
+        {
+            GetFilePath();
+        }
+        else
+        {
+            GameFolderCheck();
+        }
+    }
+
+    private void GameFolderCheck()
+    {
+       subFolders = Directory.GetDirectories(fileName);
+
+        descriptionAutoFill.gameFolders = subFolders;
+        descriptionAutoFill.GetAllFolderData();
+    }
+
+    protected void GetFilePath()
+    {
+        ChooseGamesFolder();
+    }
+}
---
+++ Assets/Scripts/Library Manager/OnUninstall.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using SimpleFileBrowser;
+using System.Collections;
+
+public class OnUninstall : MonoBehaviour
+{
+    [SerializeField] private string selectedFile;
+    [SerializeField] private GameObject folderPanel;
+
+    public void ChooseDeletionFolder()
+    {
+        // Add a new quick link to the browser (optional) (returns true if quick link is added successfully)
+        // It is sufficient to add a quick link just once
+        // Name: Users
+        // Path: C:\Users
+        // Icon: default (folder icon)
+        FileBrowser.AddQuickLink("Users", "C:\\Users", null);
+
+        // !!! Uncomment any of the examples below to show the file browser !!!
+
+        // Example 1: Show a save file dialog using callback approach
+        // onSuccess event: not registered (which means this dialog is pretty useless)
+        // onCancel event: not registered
+        // Save file/folder: file, Allow multiple selection: false
+        // Initial path: "C:\", Initial filename: "Screenshot.png"
+        // Title: "Save As", Submit button text: "Save"
+        FileBrowser.ShowLoadDialog(OnFilesSelected, null, FileBrowser.PickMode.FilesAndFolders, false, "C:\\Users\\" + System.Environment.UserName + "\\Desktop", "", "deleteFile/Folder", "Delete");
+
+    }
+
+    void OnFilesSelected(string[] filePaths)
+    {
+        // Print paths of the selected files
+        for (int i = 0; i < filePaths.Length; i++)
+        {
+            Debug.Log(filePaths[i]);
+        }
+
+        // Get the file path of the first selected file
+        selectedFile = filePaths[0];
+        DeleteGame();
+
+        // Or, copy the first file to persistentDataPath
+        FileBrowserHelpers.DeleteFile(selectedFile);
+
+    }
+
+    private void DeleteGame()
+    {
+
+        if (selectedFile != "")
+        {
+            try
+            {
+                if (Directory.Exists(selectedFile))
+                {
+                    Directory.Delete(selectedFile, true);
+                    Debug.Log("Directory deleted: " + selectedFile);
+                }
+                else
+                {
+                    Debug.Log("Directory does not exist: " + selectedFile);
+                }
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError("Error deleting directory: " + e.Message);
+            }
+        }
+        else
+        {
+            Debug.Log("No folder selected.");
+        }
+
+#if !UNITY_EDITOR
+        folderPanel.SetActive(false); // Deactivate the folder selection panel after folder selection
+#endif
+    }
+}
