e008a43ac39996a28bbc14882c3b9daeb919e7bf
Atmojomojo <rotastisch@gmail.com>
2024-03-14 16:57:33 +0100

    Fixes in main scene regarding acount

--- Assets/Scripts/Admin System/HidePassword.cs
+++ Assets/Scripts/Admin System/HidePassword.cs
@@ -6,13 +6,13 @@ using UnityEngine.UI;
 
 public class HidePassword : MonoBehaviour
 {
-    [SerializeField] private Toggle hide;
+    //[SerializeField] private Toggle hide;
     [SerializeField] private TMP_Text password, hiddenPassword;
     [SerializeField] private Color invisable, normalcolor;
     
     public void OnToggle()
     {
-        if (hide.isOn)
+        if (!gameObject.GetComponent<Toggle>().isOn)
         {
             password.color = invisable;
             hiddenPassword.color = normalcolor;
