9bd3607286869c41a848e0ffa9914cd4abd80625
Atmojomojo <rotastisch@gmail.com>
2024-03-28 09:41:47 +0100

    Start with background animations
    Made some animations in untiy
    
    Commit To transfer progress from laptop to pc

---
+++ Assets/Scripts/UI/EasterEggAnimations.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEggAnimations : MonoBehaviour
+{
+    [Tooltip("An array with all the names of the animator triggers")]
+    [SerializeField] private string[] AnimationNames;
+
+    [SerializeField] private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
