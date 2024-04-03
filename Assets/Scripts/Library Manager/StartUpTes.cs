783c8ac70c16c9b9bc0e30c866d7a84d07d74f80
SNIELSEL <niels@haverkotte.nl>
2024-04-03 13:01:11 +0200

    game startup spam fix

--- Assets/Scripts/Library Manager/StartUpTes.cs
+++ Assets/Scripts/Library Manager/StartUpTes.cs
@@ -1,12 +1,26 @@
+using System.Collections;
 using System.Diagnostics;
 using TMPro;
 using UnityEngine;
 
 public class StartUpTes : MonoBehaviour
 {
+    private bool onCoolDown;
+
     //Starts the selected game(.EXE file).
     public void OnButtonClick(TextMeshProUGUI exeFolderPath)
     {
-        Process.Start(exeFolderPath.text);
+        if (!onCoolDown)
+        {
+            onCoolDown = true;
+            Process.Start(exeFolderPath.text);
+            StartCoroutine(GameCoolDown());
+        }
+    }
+
+    public IEnumerator GameCoolDown()
+    {
+        yield return new WaitForSeconds(5);
+        onCoolDown = false;
     }
 }
