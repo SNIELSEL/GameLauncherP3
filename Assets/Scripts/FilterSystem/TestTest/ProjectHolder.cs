d8c73e72aa21f911ec306f05ce04c3c41f766785
Doetzen <Doutzenhoekstra06@gmail.com>
2024-04-04 13:47:05 +0200

    verder met filter

--- Assets/Scripts/FilterSystem/TestTest/ProjectHolder.cs
+++ Assets/Scripts/FilterSystem/TestTest/ProjectHolder.cs
@@ -1,37 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProjectHolder : MonoBehaviour
 {
     public List<Product> products = new List<Product>();
     [SerializeField] private GameObject requireCheckHolder;
-    [SerializeField] private GameObject[] requirements;
-    [SerializeField] private GameObject gameHolder;
-    [SerializeField] private GameObject[] games;
-    public List<GameObject> gameButtons = new List<GameObject>();
-    
-    public List<GameObject>  filterButtons = new List<GameObject>();
-    bool toggleOn;
+
+
+    public List<GameObject> filterButtons = new List<GameObject>();
     int totalRequirements, totalGames;
-    float waitTime = 3;
+
 
     public SetFilter setFilter = new SetFilter();
 
-    public void Start()
+    public void Init()
     {
-        foreach(Product product in products)
+        foreach (Product product in products)
         {
             product.Init();
         }
-
-        Invoke("GetListOfGames", waitTime);
     }
 
     public void GetListOfGames()
     {
         totalRequirements = requireCheckHolder.transform.childCount;
-        requirements = new GameObject[totalRequirements];
+        GameObject[] requirements = new GameObject[totalRequirements];
 
         for (int i = 0; i < totalRequirements; i++)
         {
@@ -39,124 +34,147 @@ public class ProjectHolder : MonoBehaviour
 
             products.Add(requirements[i].GetComponent<Product>());
         }
-
-        totalGames = gameHolder.transform.childCount;
-        games = new GameObject[totalGames];
-        for (int i = 0;i < totalGames; i++)
-        {
-            games[i] = gameHolder.transform.GetChild(i).gameObject;
-
-           gameButtons.Add(games[i]);
-        }
-        
     }
-    public void RatingFilter(GameObject rating)
+
+    public void RatingFilter(int rating)
     {
-        setFilter.ratingToFilterTo = rating.GetComponent<FilterButton>().rating;
+        //SLIDER AAAAAAAAAAAAAh
+        //setFilter.ratingToFilterTo = rating.GetComponent<FilterButton>().rating;
+
+       /* else
+        {
+            setFilter.ratingToFilterTo = 0;
+        }*/
     }
     public void BuildYearFilter(GameObject buildYear)
     {
-        setFilter.buildYearToFilterTo = buildYear.GetComponent<FilterButton>().buildYear;
+
+        if (buildYear.GetComponent<Toggle>().isOn)
+        {
+            setFilter.buildYearToFilterTo = buildYear.GetComponent<FilterButton>().buildYear;
+        }
+        else
+        {
+            setFilter.buildYearToFilterTo = 0;
+        }
+
     }
 
     public void StudentYearFilter(GameObject studentYear)
     {
-        setFilter.studentYearToFilterTo = studentYear.GetComponent<FilterButton>().studentYear;
+        if (studentYear.GetComponent<Toggle>().isOn)
+        {
+            setFilter.studentYearToFilterTo = studentYear.GetComponent<FilterButton>().studentYear;
+        }
+        else
+        {
+            setFilter.studentYearToFilterTo = null;
+        }
+
     }
 
     public void MultiplayerFilter(GameObject multiPlayer)
     {
-        setFilter.multiPlayToFilterTo = multiPlayer.GetComponent<FilterButton>().multiPlay;
+        if (multiPlayer.GetComponent<Toggle>().isOn)
+        {
+            setFilter.multiPlayToFilterTo = multiPlayer.GetComponent<FilterButton>().multiPlay;
+        }
+        else
+        {
+            setFilter.multiPlayToFilterTo = null;
+        }
     }
 
     public void PerspectiveFilter(GameObject perspective)
     {
-        setFilter.perspectiveToFilterTo = perspective.GetComponent<FilterButton>().perspective;
+        if (perspective.GetComponent<Toggle>().isOn)
+        {
+            setFilter.perspectiveToFilterTo = perspective.GetComponent<FilterButton>().perspective;
+        }
+        else
+        {
+            setFilter.perspectiveToFilterTo = null;
+        }
+
     }
 
     public void GenreFilter(GameObject genre)
     {
-        setFilter.genreToFilterTo = genre.GetComponent<FilterButton>().genre;
-
+        if (genre.GetComponent<Toggle>().isOn)
+        {
+            setFilter.genreToFilterTo = genre.GetComponent<FilterButton>().genre;
+        }
+        else
+        {
+            setFilter.genreToFilterTo = null;
+        }
     }
 
     public void ApplyFilter()
     {
+       
         FilterOnThis(setFilter.ratingToFilterTo, setFilter.buildYearToFilterTo, setFilter.studentYearToFilterTo, setFilter.multiPlayToFilterTo, setFilter.perspectiveToFilterTo, setFilter.genreToFilterTo);
-
-        Debug.Log(setFilter.ratingToFilterTo);
-        Debug.Log(setFilter.buildYearToFilterTo);
-        Debug.Log(setFilter.studentYearToFilterTo);
-        Debug.Log(setFilter.multiPlayToFilterTo);
-        Debug.Log(setFilter.perspectiveToFilterTo);
-        Debug.Log(setFilter.genreToFilterTo);
     }
 
-    public void FilterOnThis(int ratingToFilterTo,int buildYearToFilterTo, string studentYearToFilterTo, string multiPlayToFilterTo, string perspectiveToFilterTo, string genreToFilterTo)
+  
+    public void FilterOnThis(int ratingToFilterTo, int buildYearToFilterTo, string studentYearToFilterTo, string multiPlayToFilterTo, string perspectiveToFilterTo, string genreToFilterTo)
     {
 
         List<Product> filteredList = new List<Product>();
-        List<GameObject> games = new List<GameObject>();
-        // List<GameObject> gameList = new List<GameObject>();
+       
         foreach (Product product in products)
         {
             filteredList.Add(product);
         }
 
-        foreach(GameObject game in games)
-        {
-            foreach (Product product in products)
-            {
-                if(ratingToFilterTo > 0 && product.filter.rating != ratingToFilterTo)
-                {
-                    filteredList.Remove(product);
-                    game.SetActive(false);
-                }
-                if(buildYearToFilterTo > 0 && product.filter.buildYear != buildYearToFilterTo)
-                {
-                    filteredList.Remove(product);
-                    game.SetActive(false);
-                }
-                if(studentYearToFilterTo != "" && product.filter.studentYear != studentYearToFilterTo)
-                {
-                    filteredList.Remove(product);
-                    game.SetActive(false);
-                }
-                if(multiPlayToFilterTo != "" && product.filter.multiTag != multiPlayToFilterTo)
-                {
-                    filteredList.Remove(product);
-                    game.SetActive(false);
-                }
-                if (perspectiveToFilterTo != "" && product.filter.perspectiveTag != perspectiveToFilterTo)
-                {
-                    filteredList.Remove(product);
-                    game.SetActive(false);
-                }
-                if (genreToFilterTo != "" && product.filter.genreTag != genreToFilterTo)
-                {
-                    filteredList.Remove(product);
-                    game.SetActive(false);
-                }
-            }
-            
-            foreach (Product product in filteredList)
-            {
-                //hier moeten de laatste games overblijven
-                print(product.name);
-                foreach (GameObject gaaaame in games)
-                {
-                    game.SetActive(true);
-                }
-            }
-        }
 
+        foreach (Product product in products)
+        {
+            //rating moet even op een andere manier
+            if (ratingToFilterTo > 0 && product.filter.rating != ratingToFilterTo)
+            {
+                filteredList.Remove(product);
+            }
+            if (buildYearToFilterTo > 0 && product.filter.buildYear != buildYearToFilterTo)
+            {
+                filteredList.Remove(product);
+            }
+            if (studentYearToFilterTo != "" && product.filter.studentYear != studentYearToFilterTo)
+            {
+                filteredList.Remove(product);
+            }
+            if (multiPlayToFilterTo != "" && product.filter.multiTag != multiPlayToFilterTo)
+            {
+                filteredList.Remove(product);
+            }
+            if (perspectiveToFilterTo != "" && product.filter.perspectiveTag != perspectiveToFilterTo)
+            {
+                filteredList.Remove(product);
+            }
+            if (genreToFilterTo != "" && product.filter.genreTag != genreToFilterTo)
+            {
+                filteredList.Remove(product);
+            }
+
+        }
+        // Zet elk product uit.
+        foreach(Product g in products)
+        {
+            g.gameObject.SetActive(false);
+        }
+        //Zet elk product in de filteredLIst weer aan.
+        foreach(Product p in filteredList)
+        {
+            p.gameObject.SetActive(true);
+        }
     }
 
+   
     public void PutGamesBack()
     {
-        foreach(GameObject game in games)
+        foreach(Product product in products)
         {
-            game.SetActive(true);
+            product.GetComponent<RequirmentCheck>().gfuckingKnop.SetActive(true);
         }
     }
 }
